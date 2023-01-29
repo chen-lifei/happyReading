177281bb6f36d35522cca97273b810f1c3c4de74
fay <feilichen@yeah.net>
2023-01-29 09:20:10 +0800

    登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,41 @@
 <template>
     <div class="login-view">
-        <div class="inner-wrapper">
-            <div class="logo-wrapper">
-                <img src="@/assets/image/logo.svg" alt="悦阅" />
-                悦阅
-            </div>
-            <h2>欢迎来到悦阅！</h2>
-            <div class="tip">请先登录账号~</div>
-            <div class="form-area">
-                <input type="text" v-if="!state.isLogin" v-model="state.username" class="form-item" :class="{ 'error': state.username.length && !usernameCorrect }" placeholder="请输入用户名">
-                <input type="text" class="form-item" :class="{ 'error': !state.accountCorrect && state.account.length  }" v-model="state.account" placeholder="请输入手机号或邮箱" @blur="checkAccount">
-                <input type="password" class="form-item" :class="{ 'error': state.password.length && !passwordCorrect  }" v-model="state.password" placeholder="请输入密码">
-            </div>
-            <div class="tip-wrapper flex-between">
-                <div class="remember flex">
-                    <input type="checkbox">记住我
+        <div class="main-wrapper">
+            <img class="top-img" src="@/assets/image/login/top.svg" />
+            <img class="bottom-img" src="@/assets/image/login/bottom.svg" />
+            <div class="inner-wrapper">
+                <div class="logo-wrapper">
+                    <img src="@/assets/image/logo.svg" alt="悦阅" />
+                    悦阅
                 </div>
-                <div class="forget" v-if="state.isLogin">忘记密码?</div>
-            </div>
-            <div class="submit-btn" @click="clickSubmitBtn()">{{ state.isLogin ? '登录账号' : '创建账号' }}</div>
-            <div class="change-type" @click="changeType">{{ state.isLogin ? '注册' : '登录' }}账号</div>
-            <div class="split-wrapper">
-                <div class="line"></div>
-                <div class="text">或者</div>
-                <div class="line"></div>
-            </div>
-            <div class="quick-method flex">
-                <svg class="svg-icon" aria-hidden="true">
-                    <use xlink:href="#icon-wechat"></use>
-                </svg>
-                <svg class="svg-icon" aria-hidden="true">
-                    <use xlink:href="#icon-qq"></use>
-                </svg>
+                <h2>欢迎来到悦阅！</h2>
+                <div class="tip">请先{{ state.isLogin ? "登录" : "注册" }}账号~</div>
+                <div class="form-area">
+                    <input type="text" v-if="!state.isLogin" v-model="state.username" class="form-item" :class="{ 'error': state.username.length && !usernameCorrect }" placeholder="请输入用户名">
+                    <input type="text" class="form-item" :class="{ 'error': !state.accountCorrect && state.account.length  }" v-model="state.account" placeholder="请输入手机号或邮箱" @blur="checkAccount">
+                    <input type="password" class="form-item" :class="{ 'error': state.password.length && !passwordCorrect  }" v-model="state.password" placeholder="请输入密码">
+                </div>
+                <div class="tip-wrapper flex-between" v-if="state.isLogin">
+                    <div class="remember flex">
+                        <el-checkbox v-model="state.remember" label="记住我" />
+                    </div>
+                    <div class="forget">忘记密码?</div>
+                </div>
+                <div class="submit-btn" @click="clickSubmitBtn()">{{ state.isLogin ? "登录" : "注册" }}</div>
+                <div class="change-type" @click="changeType">{{ state.isLogin ? "注册" : "登录" }}账号</div>
+                <div class="split-wrapper">
+                    <div class="line"></div>
+                    <div class="text">或者</div>
+                    <div class="line"></div>
+                </div>
+                <div class="quick-method flex">
+                    <svg class="svg-icon" aria-hidden="true">
+                        <use xlink:href="#icon-wechat"></use>
+                    </svg>
+                    <svg class="svg-icon" aria-hidden="true">
+                        <use xlink:href="#icon-qq"></use>
+                    </svg>
+                </div>
             </div>
         </div>
     </div>
@@ -47,6 +51,7 @@
 
     let state = reactive({
         isLogin: true,
+        remember: false,
         username: '',
         account: '',
         password: '',
@@ -84,8 +89,7 @@
         if (!passwordCorrect) {
             return window.alert('密码需大于6位且小于12位');
         }
-        
-        
+
         if (state.isLogin) {
             // 登录
             store.dispatch('login', {
@@ -128,20 +132,41 @@
         justify-content: center;
         width: 100%;
         height: 100%;
+        background: #FFFFFF;
+
+        .main-wrapper {
+            position: relative;
+
+            .top-img {
+                position: absolute;
+                left: -60px;
+                top: -60px;
+                inline-size: 240px;
+            }
+
+            .bottom-img {
+                position: absolute;
+                right: -60px;
+                bottom: -60px;
+                inline-size: 240px;
+            }
+        }
 
         .inner-wrapper {
+            position: relative;
             width: 440px;
             padding: 40px 30px;
             color: #33303C;
             border-radius: 8px;
             background-color: #FFFFFF;
-            box-shadow: 0 4px 10px 0 rgba(140, 171, 145, .5);
+            box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.1);
+            z-index: 2;
 
             .logo-wrapper {
                 display: flex;
                 align-items: center;
                 justify-content: center;
-                margin-bottom: 20px;
+                margin-bottom: 30px;
                 font-size: 20px;
                 color: #000000;
                 img {
@@ -199,7 +224,7 @@
             }
 
             .submit-btn {
-                margin: 14px 0;
+                margin: 20px 0;
                 padding: 8px 0;
                 color: #FFFFFF;
                 border-radius: 4px;
@@ -207,11 +232,12 @@
                 background: var(--mainColor);
                 cursor: pointer;
                 &:hover {
-                    box-shadow: 0 4px 10px 0 rgba(140, 171, 145, .5);
+                    background: var(--mainHoverColor);
                 }
             }
 
             .change-type {
+                font-size: 14px;
                 text-align: center;
                 color: var(--mainColor);
                 &:hover {
@@ -223,7 +249,7 @@
             .split-wrapper {
                 display: flex;
                 align-items: center;
-                margin: 15px 0;
+                margin: 20px 0;
                 .line {
                     flex: 1;
                     height: 1px;
@@ -236,8 +262,12 @@
             }
 
             .quick-method {
-                .svg-icon:first-of-type {
-                    margin-right: 2px;
+                .svg-icon {
+                    font-size: 34px;
+                    cursor: pointer;
+                    &:first-of-type {
+                        margin-right: 6px;
+                    }
                 }
             }
         }
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // 配置端口号
+    // server: {
+    //     port: 3008,
+    // },
     plugins: [vue()],
     resolve: {
         alias: {
