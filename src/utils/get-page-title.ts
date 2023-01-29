536ef25ed169015e62683743fb0112e8044695e7
chenlifei <57827937+chen-lifei@users.noreply.github.com>
2023-01-29 09:48:18 +0800

    Delete src directory

--- src/utils/get-page-title.ts
+++
@@ -1,8 +0,0 @@
-const title = '悦阅';
-
-export default function getPageTitle(pageTitle) {
-  if (pageTitle) {
-    return `${pageTitle} - ${title}`
-  }
-  return `${title}`
-}
--- src/utils/validate.ts
+++
@@ -1,9 +0,0 @@
-export function validatePhone(value) {
-    const reg = /^[1][3-9][0-9]{9}$/;
-    return reg.test(value);
-}
-
-export function validateEmail(value) {
-    const reg = /^([a-zA-Z0-9]+[-_\.]?)+@[a-zA-Z0-9]+\.[a-z]+$/;
-    return reg.test(value);
-}
