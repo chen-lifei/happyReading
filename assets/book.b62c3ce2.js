d25f1adcf70d94ec48809cdecc96fc8033513817
fay <feilichen@yeah.net>
2023-01-31 10:18:10 +0800

    更新

---
+++ assets/book.b62c3ce2.js
@@ -0,0 +1,1 @@
+import{z as o}from"./index.36d22617.js";function e(){return o({method:"GET",url:"/books/category"})}function s(t){return o({method:"POST",url:"/books/bookList",data:t})}export{e as a,s as f};
