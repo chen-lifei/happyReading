8fd804b48145b0857e53d62996977d79f313a084
fay <feilichen@yeah.net>
2023-02-02 11:41:53 +0800

    update

---
+++ assets/book.4e2e284f.js
@@ -0,0 +1,1 @@
+import{r as o}from"./axios.8e9c47e0.js";function e(){return o({method:"GET",url:"/books/category"})}function s(t){return o({method:"POST",url:"/books/bookList",data:t})}export{e as a,s as f};
