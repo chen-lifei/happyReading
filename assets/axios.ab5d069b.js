3916962d8e70fea7deedb08834e61f9415450818
fay <feilichen@yeah.net>
2023-02-07 10:13:44 +0800

    update

---
+++ assets/axios.ab5d069b.js
@@ -0,0 +1,1 @@
+import{aa as t}from"./index.cc8f0ae0.js";function s(e){return t.create({baseURL:"http://localhost:3000",timeout:2e4})(e)}export{s as r};
---
+++ assets/book.252f883e.js
@@ -0,0 +1,1 @@
+import{r as o}from"./axios.ab5d069b.js";function e(){return o({method:"GET",url:"/books/category"})}function s(t){return o({method:"POST",url:"/books/bookList",data:t})}export{e as a,s as f};
